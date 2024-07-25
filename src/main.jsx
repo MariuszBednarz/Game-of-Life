fd1e77e51ed84d6cc0d9d079ebb7762f28b5c64c
MariuszBednarz <mmbednarz90@gmail.com>
2024-07-25 22:00:23 +0200

    config check

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import { BrowserRouter } from "react-dom";
 import { GameProvider } from "./context/GameContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GameProvider>
-      <App />
-    </GameProvider>
+    <BrowserRouter basename="/game-of-life/">
+      <GameProvider>
+        <App />
+      </GameProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
